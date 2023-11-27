d86969c05aff798bbe61fd71d4496e562f7f748b
ashley773877 <mirandaashley16@gmail.com>
2023-11-27 14:03:10 -0500

    part 1 check

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 
 // part 3
-const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26\n"
-
-let currentcell = 1;
+// const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26\n"
+/*let currentcell = 1;
 let cell1 = '';
 let cell2 = '';
 let cell3 = '';
-let cell4 = '';
+let cell4 = ''; 
 
 
 
@@ -38,14 +37,23 @@ if (csv[i] === ",") {
     cell4 += csv[i];
 }
     
-}
+} */
 
 // part 1 
 for (let i = 1; i <= 100; i++) {
-    if(i % 3 === 0 && i % 5 === 0) {
+    if(i % 3 === 0 && i % 5 === 0) { // check to see if # are divisible by both 3 and 5
         console.log("fizz buzz");
     
 }
+else if (i % 3 === 0){ // checking if the number is divisible by 3
+    console.log("fizz");
+}
 
+else if (i % 5 === 0) { // checking if the number is divisble by 5
+    console.log("buzz");
+}
+else {
+    console.log(i); // if the number isnt divisble by 3 and 5 then just log the number that is 
+}
 
 }
